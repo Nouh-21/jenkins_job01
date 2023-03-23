dffe044b460f2f64767f7c54c2704b9d8f979350
Nouh-21 <73238928+Nouh-21@users.noreply.github.com>
2023-03-23 23:36:26 +0100

    001

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def build(){
+   sh "this a building stage"
+   sh "mvn install"
+}
+def test(){
+  echo 'this is a building  stage'
+  echo "this is a number of version ${params.VERSION}"
+}
+
+def deploye(){
+  echo 'this is a deploo'
+  echo " this is a credentials ${CREDENTAILS_USER}"
+}
+
+return this
